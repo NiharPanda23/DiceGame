4a5383163efb1e45fa0b366770388eac5a8b3d37
Nihar Ranjan Panda <110398042+NiharPanda23@users.noreply.github.com>
2023-03-31 13:16:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-
 var randomNumber1 = Math.round(Math.random() * 6) + 1;
-
 var randomDiceImage = "dice" + randomNumber1 + ".png";
-
 var randomImageSource = "Images/"+ randomDiceImage;
-
 var image1 = document.querySelectorAll("img")[0]
-
-
-
 image1.setAttribute("src", randomImageSource);
 
 // for second Image
 var randomNumber2 = Math.round(Math.random() * 6) ;
-
 var randomDiceImage1 = "dice" + randomNumber2 + ".png";
-
 var randomImageSource1 = "Images/"+ randomDiceImage1;
-
 var image2 = document.querySelectorAll("img")[1];
-
 image2.setAttribute("src", randomImageSource1);
 
 if (randomNumber1 > randomNumber2){
